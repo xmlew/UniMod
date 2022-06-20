327c4660820452977fd0ddb7313132aa221920d5
Xuan Ming <77575992+xmlew@users.noreply.github.com>
2022-06-20 21:18:31 +0800

    Update auth.js

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,10 +1,32 @@
-const users = require('../models/users');
+
+const user = require('../models/users');
 // user registration, login
 // Primary purpose of login feature is to save module data. Difficult to use cookies so currently trying this out
+const loginUser = async (req, res) => {
+    const { username, password } = req.body;
+  
+    const user = await User.findOne({
+      username: { $regex: new RegExp('^' + username + '$', 'i') },
+    });
+  
+    if (!user) {
+      return res
+        .status(400)
+        .send({ message: 'No account with this username has been registered.' });
+    }
+
+    if (user.password == password) {
+      return res.status(401).send({ message: 'Invalid username or password.' });
+    }
+
+    res.status(200).json({
+      username: user.username
+    });
+  };
 
 //CREATE USER: LOGIC
 /* Username: Between 3 and 20 chars
-   Password: Between 8 and 20 chars
+   Password: >8 chars
 */
 const registerUser = async (req, res) => {
     const {username, password} = req.body;
@@ -24,9 +46,22 @@ const registerUser = async (req, res) => {
             message: `Username length must be in range of 3 to 20.`
         })
     }
-};
 
-//LOGIN TO USER:
-/* If wrong user/pass: Throw error
-   Else: Sign user in
-*/
+    if (!password || password.length < 6) {
+        return res
+          .status(400)
+          .send({ message: 'Password needs to be atleast 8 characters long.' });
+    };
+    const user = new user({
+        username,
+        password,
+        Array,
+    })
+
+    const savedUser = await user.save();
+    res.status(200).json({
+        username: savedUser.username
+    });
+}
+
+module.exports = {registerUser, loginUser}
