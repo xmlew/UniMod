78ae35b63e5d17ed67a74d5c14104b9e9a74d882
Xuan Ming <77575992+xmlew@users.noreply.github.com>
2022-06-25 16:29:04 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,5 @@
-//let mongoURI = `mongodb://localhost:27017`
 const express = require("express");
 const mongoose = require("mongoose");
-//const help = require("./controllers/modules")
-const { modules } = require("./models/modules");
-const { moduleSearch } = require("./models/modsearch")
-const users = require("./models/users");
 
 const {moduleSubs, dataDisplay, getCourseStudentData, modPopularity} = require("./controllers/modules");
 
@@ -12,11 +7,9 @@ const {moduleSubs, dataDisplay, getCourseStudentData, modPopularity} = require("
 
 const app = express();
 app.use(express.json());
-
 const PORT = 3002;
 
 mongoose.Promise = global.Promise;
-
 const connectToDB = async () => {
     try {
         //await mongoose.connect('mongodb://127.0.0.1:27017/UniMod',
@@ -31,51 +24,11 @@ const connectToDB = async () => {
         console.error(`Error while connecting to MongoDB: `, error.message);
     }
 };
-
 connectToDB();
 
-//find a piece of data then log the result. Currently testing with one module.
-const findModByCode = (moduleCode) => {
-    modules.find({'Module Code': moduleCode}).then(result => {
-        console.log(result[0])
-        if (result.count == 0) {
-            return "Not found"
-        } else if (result[0]['UG'] == "x") {
-            return "Not applicable for undergraduates"
-        } else if (result[0]['UG'] == "-") {
-            return "Is oversubscribed"
-        } else if (int(result[0]['UG']) < 10) {
-            return "Is popular"
-        } else if (int(result[0]['UG']) < 25) {
-            return "Has vacancies"
-        } else {
-            return "Has more than enough vacancies"
-        }
-    });
-}
-
 //checks if there is an existing user with the same username in the database. If there is, log that a user already exists.
-const registerUser = async (name, pass) => {
-    let newUser = {
-        username: name,
-        password: pass
-    };
 
-    const existingUser = await users.findOne({
-        username: { $regex: new RegExp('^' + username + '$', 'i')},
-    });
-
-    if (users.find({username: name}).count() != 0) {
-        console.log("User already exists");
-        return
-
-    } else {
-        users.insertMany(newUser);
-        console.log("new user added")
-    };
-};
-
-app.get('/', function (req, res, next) {
+app.get('/', function (req, res) {
     return res.send('UniMod API')
 });
 
@@ -84,14 +37,8 @@ app.get('/modsearch/:code', moduleSubs);
 app.get('/display/:code', dataDisplay);
 app.get('/courseData/:course', getCourseStudentData);
 app.get('/modtakers/:code', modPopularity);
-//app.get('/modrec/:code', moduleSearch)
-
-//router.post('/register', )
-//users.insertMany(user);
-
-//users.find().then(result => console.log(result))
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}...`)
 })
+
