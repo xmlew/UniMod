d629d6615b401e4f6e7591290ebc964482d9c79b
Joel Lo <9198061+aerodromic@users.noreply.github.com>
2022-07-18 01:22:01 +0800

    Update RegisterForm.js

--- src/sections/auth/register/RegisterForm.js
+++ src/sections/auth/register/RegisterForm.js
@@ -31,7 +31,7 @@ export default function RegisterForm() {
     },
     validationSchema: RegisterSchema,
     onSubmit: () => {
-      navigate('/dashboard', { replace: true });
+      navigate('/login', { replace: true });
     },
   });
 
