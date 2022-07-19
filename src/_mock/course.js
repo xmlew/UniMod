520579445503381abd12b66adc6149adca03305d
Joel Lo <9198061+aerodromic@users.noreply.github.com>
2022-07-20 05:06:33 +0800

    Courses Components

---
+++ src/_mock/course.js
@@ -0,0 +1,26 @@
+import { faker } from '@faker-js/faker';
+import { sample } from 'lodash';
+
+// ----------------------------------------------------------------------
+
+const courses = [...Array(25)].map((_, index) => ({
+  id: faker.datatype.uuid(),
+  avatarUrl: `/static/mock-images/avatars/avatar_${index + 1}.jpg`,
+  name: sample([
+    "apple",
+    "banana",
+    "pear",
+  ]),
+  code: sample([
+    "CS1010",
+    "CS1101S",
+    "CS1010E",
+    "CS1010S",
+  ]),
+  faculty: sample([
+    "CS",
+    "BZA",
+  ]),
+}));
+
+export default courses;
--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -1,5 +1,4 @@
 import { filter } from 'lodash';
-import { sentenceCase } from 'change-case';
 import { useState } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 // material
@@ -20,22 +19,19 @@ import {
 } from '@mui/material';
 // components
 import Page from '../components/Page';
-import Label from '../components/Label';
 import Scrollbar from '../components/Scrollbar';
 import Iconify from '../components/Iconify';
 import SearchNotFound from '../components/SearchNotFound';
-import { UserListHead, UserListToolbar, UserMoreMenu } from '../sections/@dashboard/user';
-// mock
-import USERLIST from '../_mock/user';
+import { CourseListHead, CourseListToolbar, CourseMoreMenu } from '../sections/@dashboard/course';
+// Data array for COURSELIST
+import COURSELIST from '../_mock/course';
 
 // ----------------------------------------------------------------------
 
 const TABLE_HEAD = [
-  { id: 'courseCode', label: 'Course Code', alignRight: false },
-  { id: 'couseName', label: 'Course Name', alignRight: false },
+  { id: 'name', label: 'Course Name', alignRight: false },
+  { id: 'code', label: 'Course Code', alignRight: false },
   { id: 'faculty', label: 'Faculty', alignRight: false },
-  { id: 'isVerified', label: 'Verified', alignRight: false },
-  { id: 'status', label: 'Status', alignRight: false },
   { id: '' },
 ];
 
@@ -65,12 +61,12 @@ function applySortFilter(array, comparator, query) {
     return a[1] - b[1];
   });
   if (query) {
-    return filter(array, (_user) => _user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+    return filter(array, (_course) => _course.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
   }
   return stabilizedThis.map((el) => el[0]);
 }
 
-export default function User() {
+export default function Course() {
   const [page, setPage] = useState(0);
 
   const [order, setOrder] = useState('asc');
@@ -91,7 +87,7 @@ export default function User() {
 
   const handleSelectAllClick = (event) => {
     if (event.target.checked) {
-      const newSelecteds = USERLIST.map((n) => n.name);
+      const newSelecteds = COURSELIST.map((n) => n.name);
       setSelected(newSelecteds);
       return;
     }
@@ -126,42 +122,45 @@ export default function User() {
     setFilterName(event.target.value);
   };
 
-  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - USERLIST.length) : 0;
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - COURSELIST.length) : 0;
 
-  const filteredUsers = applySortFilter(USERLIST, getComparator(order, orderBy), filterName);
+  const filteredCourses = applySortFilter(COURSELIST, getComparator(order, orderBy), filterName);
 
-  const isUserNotFound = filteredUsers.length === 0;
+  const isCourseNotFound = filteredCourses.length === 0;
 
   return (
-    <Page title="Courses">
+    <Page title="Course">
       <Container>
         <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
           <Typography variant="h4" gutterBottom>
-            Courses
+            Course
           </Typography>
           <Button variant="contained" component={RouterLink} to="#" startIcon={<Iconify icon="eva:plus-fill" />}>
             New Course
           </Button>
+          <Button variant="contained" component={RouterLink} to="#" startIcon={<Iconify icon="eva:minus-fill" />}>
+            Delete Course
+          </Button>
         </Stack>
 
         <Card>
-          <UserListToolbar numSelected={selected.length} filterName={filterName} onFilterName={handleFilterByName} />
+          <CourseListToolbar numSelected={selected.length} filterName={filterName} onFilterName={handleFilterByName} />
 
           <Scrollbar>
             <TableContainer sx={{ minWidth: 800 }}>
               <Table>
-                <UserListHead
+                <CourseListHead
                   order={order}
                   orderBy={orderBy}
                   headLabel={TABLE_HEAD}
-                  rowCount={USERLIST.length}
+                  rowCount={COURSELIST.length}
                   numSelected={selected.length}
                   onRequestSort={handleRequestSort}
                   onSelectAllClick={handleSelectAllClick}
                 />
                 <TableBody>
-                  {filteredUsers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-                    const { id, name, role, status, company, avatarUrl, isVerified } = row;
+                  {filteredCourses.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                    const { id, name, faculty, code, avatarUrl} = row;
                     const isItemSelected = selected.indexOf(name) !== -1;
 
                     return (
@@ -169,7 +168,7 @@ export default function User() {
                         hover
                         key={id}
                         tabIndex={-1}
-                        role="checkbox"
+                        faculty="checkbox"
                         selected={isItemSelected}
                         aria-checked={isItemSelected}
                       >
@@ -184,17 +183,11 @@ export default function User() {
                             </Typography>
                           </Stack>
                         </TableCell>
-                        <TableCell align="left">{company}</TableCell>
-                        <TableCell align="left">{role}</TableCell>
-                        <TableCell align="left">{isVerified ? 'Yes' : 'No'}</TableCell>
-                        <TableCell align="left">
-                          <Label variant="ghost" color={(status === 'banned' && 'error') || 'success'}>
-                            {sentenceCase(status)}
-                          </Label>
-                        </TableCell>
+                        <TableCell align="left">{code}</TableCell>
+                        <TableCell align="left">{faculty}</TableCell>
 
                         <TableCell align="right">
-                          <UserMoreMenu />
+                          <CourseMoreMenu />
                         </TableCell>
                       </TableRow>
                     );
@@ -206,7 +199,7 @@ export default function User() {
                   )}
                 </TableBody>
 
-                {isUserNotFound && (
+                {isCourseNotFound && (
                   <TableBody>
                     <TableRow>
                       <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
@@ -222,7 +215,7 @@ export default function User() {
           <TablePagination
             rowsPerPageOptions={[5, 10, 25]}
             component="div"
-            count={USERLIST.length}
+            count={COURSELIST.length}
             rowsPerPage={rowsPerPage}
             page={page}
             onPageChange={handleChangePage}
