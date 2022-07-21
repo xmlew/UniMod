de652cd8101bf45482ea5223b779cf3e87bbf927
Joel Lo <9198061+aerodromic@users.noreply.github.com>
2022-07-21 10:40:36 +0800

    Update account.js
    Change the display name to reflect mock data.

--- src/_mock/account.js
+++ src/_mock/account.js
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------------
 
 const account = {
-  displayName: 'Jaydon Frankie',
+  displayName: 'Joel Lo',
   email: 'demo@minimals.cc',
   photoURL: '/static/mock-images/avatars/avatar_default.jpg',
 };
